5ff7305fb7a5a8dac06517479a92ccef5c19a13c
Jakub Gawenda <gawenda.jakub1@gmail.com>
2020-11-25 16:33:19 +0100

    finished parser stuff

--- spell_parser.py
+++ spell_parser.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+# to be honest, it's not really that complicated
+
 # bunch of stuff, I don't really know why I have 'os' imported
 import os
+import csv
+import math
 import re
 import xml.etree.ElementTree as ET
 
@@ -27,13 +31,13 @@ class Spell:
     # LOTS OF SHIT
     # probably doesn't need to be initialized, but well. We'll see.
     def __init__(self):
-        self.id = ""
-        self.name = ""
-        self.description = ""
-        self.xml_file = ""
-        self.type = ""
-        self.mana_cost = 0
-        self.uses = 0
+        self.id = None
+        self.name = None
+        self.description = None
+        self.xml_file = None
+        self.type = None
+        self.mana_cost = None
+        self.uses = None
         # list for all damage types (if it = 0, won't be included)
         self.damage = [0, # IMPACT DAMAGE
                        0, # SLICE DAMAGE
@@ -43,7 +47,7 @@ class Spell:
                        0, # EXPLOSION DAMAGE
                        ]
         # list for all damage modifier types (if it = 0, won't be included)
-        # I don't think it needs that much 
+        # this one is probably needed
         self.damage_modifier = [0, # IMPACT DAMAGE
                        0, # SLICE DAMAGE
                        0, # DRILL DAMAGE
@@ -51,7 +55,7 @@ class Spell:
                        0, # ELECTRICITY DAMAGE
                        0, # EXPLOSION DAMAGE
                        ]
-        self.radius = 0
+        self.radius = None
         # radius for all damage types:
         # same here
         # self.radius = [0, # IMPACT DAMAGE
@@ -61,27 +65,17 @@ class Spell:
         #                0, # ELECTRICITY DAMAGE
         #                0, # EXPLOSION DAMAGE
         #                ]
-        self.spread = 0
-        self.speed_min = 0
-        self.speed_max = 0
-        self.lifetime = 0
-        self.speed_modifier = 0
-        self.cast_delay = 0
-        self.recharge_time = 0
-        self.spread_modifier = 0
-        self.critical_chance_modifier = 0
-        self.terrain_destroy = 0
-        self.liquid_destroy = 0
-
-    def printInfo(self):
-        print("SPELL NAME:", self.id)
-        print("Mana cost:", self.mana_cost)
-        print("Max uses:", self.uses)
-        print("Impact damage:", self.damage[0])
-
-        print("Cast delay:", self.cast_delay)
-        print("Recharge time:", self.recharge_time)
-        print('\n')
+        self.spread = None
+        self.speed_min = None
+        self.speed_max = None
+        self.lifetime = None
+        self.speed_modifier = None
+        self.cast_delay = None
+        self.recharge_time = None
+        self.spread_modifier = None
+        self.critical_chance_modifier = None
+        self.terrain_destroy = None
+        self.liquid_destroy = None
 
     # @brief: parses a given Lua code to extract variables and their values
     # @param spell_block: block extracted by getSpellBlock()
@@ -92,6 +86,7 @@ class Spell:
         keys = []
         values = []
 
+        # going through each line in spell_block
         for item in spell_block:
             split = item.split("=")
 
@@ -103,9 +98,14 @@ class Spell:
                     values.append("".join(temp[0]).lower())
                     keys.append(split[0].strip())
 
-                # values.append( "".join(re.findall(quotes_re, split[1].strip())).lower() )
+            elif ( split[0].strip() == "type" ):
+                # temp = re.findall(r'_([A-Z]+),', split[1].strip())
+                # values.append(temp[0].lower())
+
+                values.append(split[1].strip())
+                keys.append(split[0].strip())
             else:
-                temp = re.findall(r'([-+*/]? [0-9.]+)', split[1])
+                temp = re.findall(r'([-+]? [0-9.]+)', split[1])
 
                 if ( temp ):
                     # workaround for comments in Lua files
@@ -114,40 +114,43 @@ class Spell:
                     temp[0] = temp[0].replace(" ", "")
                     values.append("".join(temp[0]).lower())
                     keys.append(split[0].strip())
-                # values.append( "".join(re.findall(r'[^a-z]([-+]? [0-9]+)', split[1].strip())).lower() )
 
         spell_dict = dict(zip(keys, values))
 
         #BUNCH OF IFS BECAUSE FUCK YOU
+        # probably could've been done with dicts, but oh well
         if ( "id" in spell_dict ):
             self.id = spell_dict["id"]
         if ( "name" in spell_dict ):
             self.name = spell_dict["name"]
+        if ( "description" in spell_dict):
+            self.description = spell_dict["description"]
         if ( "related_projectiles" in spell_dict ):
             self.xml_file = spell_dict["related_projectiles"]
         if ( "type" in spell_dict ):
             self.type = spell_dict["type"]
         if ( "mana" in spell_dict ):
-            self.mana_cost = spell_dict["mana"]
+            self.mana_cost = int(spell_dict["mana"])
         if ( "max_uses" in spell_dict ):
-            self.uses = spell_dict["max_uses"]
+            self.uses = int(spell_dict["max_uses"])
+        else:
+            self.uses = -1
         if ( "c.damage_projectile_add" in spell_dict ):
-            self.damage_modifier[0] = spell_dict["c.damage_projectile_add"]
+            self.damage_modifier[0] = float(spell_dict["c.damage_projectile_add"])*25
         if ( "c.damage_explosion_add" in spell_dict ):
             self.damage_modifier[5] = spell_dict["c.damage_explosion_add"]
         if ( "c.damage_electricity_add" in spell_dict ):
-            self.damage_modifier[4] = spell_dict["c.damage_electricity_add"]
+            self.damage_modifier[4] = float(spell_dict["c.damage_electricity_add"])*25
         if ( "c.fire_rate_wait" in spell_dict ):
-            self.cast_delay = spell_dict["c.fire_rate_wait"]
+            self.cast_delay = round(float(spell_dict["c.fire_rate_wait"])/60, 2)
         if( "current_reload_time" in spell_dict ):
-            self.recharge_time = spell_dict["current_reload_time"]
+            self.recharge_time = round(float(spell_dict["current_reload_time"])/60, 2)
         if ( "c.spread_degrees" in spell_dict ):
-            self.spread_modifier = spell_dict["c.spread_degrees"]
+            self.spread_modifier = float(spell_dict["c.spread_degrees"])
         if ( "c.speed_multiplier" in spell_dict ):
-            self.speed_multiplier = spell_dict["c.speed_multiplier"]
+            self.speed_multiplier = float(spell_dict["c.speed_multiplier"])
         if ( "c.critical_damage_chance" in spell_dict ):
-            self.critical_chance_modifier = spell_dict["c.critical_damage_chance"]
-
+            self.critical_chance_modifier = int(spell_dict["c.critical_damage_chance"])
 
         return 0
 
@@ -159,6 +162,8 @@ class Spell:
                 print ("duplicate attributes in:", self.xml_file)
                 print (e)
                 return 1
+
+            # projectile_dict = {}
 
             for projectile in root.iter("ProjectileComponent"):
                 stat_dict = projectile.attrib
@@ -173,7 +178,7 @@ class Spell:
                 explosion_dict = explosion.attrib
 
                 if ( "damage" in explosion_dict ):
-                    self.damage[5] = float(explosion_dict["damage"])
+                    self.damage[5] = float(explosion_dict["damage"])*100
                 if ( "explosion_radius" in explosion_dict ):
                     self.radius = float(explosion_dict["explosion_radius"])
                 if ( "hole_enabled" in explosion_dict ):
@@ -197,8 +202,15 @@ class Spell:
 
         return 1
 
+    def printInfo(self):
+        print(self.__dict__)
+        print('\n')
 
-
+    def printToCSV(self, filename):
+        print(list(self.__dict__.keys()))
+        # with open(filename, 'a', newline="") as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     writer.writerows(list(self.__dict__.keys()))
 
 
 path_to_data = "/home/gawenda/USB/Noita/"
@@ -224,6 +236,6 @@ with open(path_to_data + path_to_gun + file_name, "r") as f:
 
             test.parseSpellBlock(spell_block)
             test.parseXML()
-            test.printInfo()
+            test.printToCSV("spells.csv")
 
         temp = f.readline()
